a44fcd545a104ca30743df7b9ec50dc745cc756f
William Schramm <william.schramm11@gmail.com>
2022-02-02 00:16:24 -0500

    Added new setup wtih src folder and SASS 7-1 structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Script from "next/script";
 import styles from "../styles/Home.module.css";
 import { attributes, react as HomeContent } from "../content/home.md";
 
