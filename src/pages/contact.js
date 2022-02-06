b5c315aa75416837b001c95c8ef643ede0411bff
William Schramm <william.schramm11@gmail.com>
2022-02-06 18:14:10 -0500

    Added svgs to page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,27 @@
 import Layout from "../components/Layout";
 
+import EmailIcon from "../components/svg/Email";
+import BxlTwitterIcon from "../components/svg/Twitter";
+
 export default function Contact({ title, description, ...props }) {
   return (
     <>
       <Layout pageTitle={`${title} | Contact Me`} pageDescription={description}>
         <div className="contact-layout">
-          <div className="biography">{/* Put biography here */}</div>
-          <div className="headshot">{/* put headshot here*/}</div>
+          <div className="contacts">
+            <div className="contact-item">
+              <BxlTwitterIcon width="5rem" height="5rem" />
+              @TheYoungAbraxan
+            </div>
+            <div className="contact-item">
+              <EmailIcon width="5rem" height="5rem" />
+              abbyhickman98@gmail.com
+            </div>
+          </div>
+          <div className="contact-cv">
+            <a href="/Abigail_Hickman_Resume_Winter_2022.pdf">Here</a>'s my
+            resume.
+          </div>
         </div>
       </Layout>
     </>
