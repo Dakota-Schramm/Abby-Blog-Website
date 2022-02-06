cc410b4375f38dc4b48fb273b90bde8decb43f5e
William Schramm <william.schramm11@gmail.com>
2022-02-06 18:12:31 -0500

    Added components to contact page.

---
+++ src/components/svg/Email.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+function EmailIcon(props) {
+  return (
+    <svg
+      viewBox="0 0 48 48"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M40 8H8c-2.21 0-3.98 1.79-3.98 4L4 36c0 2.21 1.79 4 4 4h32c2.21 0 4-1.79 4-4V12c0-2.21-1.79-4-4-4zm0 8L24 26 8 16v-4l16 10 16-10v4z" />
+    </svg>
+  );
+}
+
+export default EmailIcon;
