e8c178c090833595ea804bda5eee941c550d7a2e
William Schramm <william.schramm11@gmail.com>
2022-02-14 22:47:05 -0500

    Added read more tag to post entries in Post List.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,15 +4,23 @@ export default function PostList({ posts }) {
   if (posts === "undefined") return null;
 
   return (
-    <div>
+    <div className="post-list">
       {!posts && <div>No posts!</div>}
       {posts &&
         posts.map((post) => {
+          /* Need to update so that
+              date string is formatted
+              frontmatter.title is properly formatted for route
+          */
+
           const d = new Date(post.frontmatter.date);
+          const firstParagraph = post.markdownBody.split("\n")[0];
 
           return (
             <div className="post-entry" key={post.slug}>
-              <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
+              <Link
+                href={`/posts/${encodeURIComponent(post.frontmatter.title)}`}
+              >
                 <a className="post-title">{post.frontmatter.title}</a>
               </Link>
               <div className="post-sub-title">
@@ -20,8 +28,11 @@ export default function PostList({ posts }) {
               </div>
               <div
                 className="post-content"
-                dangerouslySetInnerHTML={{ __html: post.markdownBody }}
+                dangerouslySetInnerHTML={{ __html: firstParagraph }}
               ></div>
+              <Link href={`/posts/${encodeURIComponent(post.slug)}`}>
+                <a>Read more...</a>
+              </Link>
             </div>
           );
         })}
