844442371ed98feb0fbbbd59a4057b7027c711b6
Dakota Schramm <william.schramm11@gmail.com>
2022-04-10 07:39:17 -0400

    Smoke test for blogposts passed.

---
+++ src/pages/posts/posts.spec.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Blog from '.'
+import { setConfig } from 'next/config';
+
+
+
+const { faker } = require('@faker-js/faker');
+
+function generatePost() {
+    const title = faker.lorem.words(7)
+    const date = faker.date.past()
+    const body = `${[...Array(3)].map(() => `<p>${faker.lorem.sentences(10)}</p>` ).join("\n")}` 
+
+    return {
+        frontmatter: {
+            title: title,
+            date: date
+        },
+        markdownBody: body
+    }
+}
+
+describe("post-list with mock data", () => {
+    // it("post-list renders", () => {
+
+        // mount(<Blog posts={posts} />)
+    // })
+    
+    it('removes the text from __NEXT_DATA__', () => {
+        cy.visit('localhost:3000/posts', {
+            onBeforeLoad: (win) => {
+                let nextData
+
+                var posts = []; 
+                for(let i=0; i<50; i++) {
+                    posts.push(generatePost())
+                }
+
+                Object.defineProperty(win, '__NEXT_DATA__', {
+                    set(o) {
+                        console.log('setting __NEXT_DATA__', o)
+                        // here is our change to modify the injected parsed data
+                        o.props.pageProps.posts = posts 
+                        nextData = o
+                    },
+                    get() {
+                        return nextData
+                    },
+                })
+            },
+        })
+    })
+})
