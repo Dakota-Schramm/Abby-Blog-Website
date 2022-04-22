7c9f645f35d125926de7f71d9de4ffa6b515984f
Dakota Schramm <william.schramm11@gmail.com>
2022-04-21 21:59:54 -0400

    Fixed to allow test colocation.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pageExtensions: ["*.page.js","*.page.jsx"],
+  pageExtensions: ['page.jsx, page.js'],
   reactStrictMode: true,
   webpack: (cfg) => {
     cfg.module.rules.push({
